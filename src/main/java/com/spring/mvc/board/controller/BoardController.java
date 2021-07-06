715a0d8827cede3a504f6260f8dd0f9b84aabf70
ijieun <mz_ljieun@woowafriends.com>
2021-07-06 14:15:53 +0900

    코드리뷰 - void로 변환

--- src/main/java/com/spring/mvc/board/controller/BoardController.java
+++ src/main/java/com/spring/mvc/board/controller/BoardController.java
@@ -1,27 +1,25 @@
 package com.spring.mvc.board.controller;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@RequiredArgsConstructor
 @Controller
 public class BoardController {
 
   @GetMapping("/board")
-  public String board(){
+  public String board() {
     return "board";
   }
 
   @GetMapping("/register")
-  public String register(){
+  public String register() {
     return "register";
   }
 
   @GetMapping("/detail/{postId}")
-  public String detail(@PathVariable int postId, Model model){
+  public String detail(@PathVariable int postId, Model model) {
     model.addAttribute("postId", postId);
     return "/detail/detail";
   }
